f64244e4c62aee81a0349adfc03c9828d7d250d6
Max MacNeill <d3n2c@unb.ca>
2024-08-11 13:08:21 -0300

    Add requirements and initial settings

---
+++ settings.py
@@ -0,0 +1,7 @@
+DBHOST = 'placeholder'
+DBPORT = 'placeholder'
+DBUSER = 'placeholder'
+DBPASSWD = 'placeholder'
+DBDATABASE = 'placeholder'
+# The following line must be the integer ID of the desired admin role.
+ADMINROLE = placeholderINT
