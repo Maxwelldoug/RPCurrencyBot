7a815888c4d307a1ef3bb10820775b0d49f90429
Max MacNeill <d3n2c@unb.ca>
2024-08-11 20:45:57 -0300

    Add token to settings

--- settings.py
+++ settings.py
@@ -5,3 +5,5 @@ DBPASSWD = 'placeholder'
 DBDATABASE = 'placeholder'
 # The following line must be the integer ID of the desired admin role.
 ADMINROLE = placeholderINT
+# Your Bot's Discord Token. Do not use a user token.
+BOTTOKEN = 'placeholder'
