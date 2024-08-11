dc467124fd42c0500d4030b4ba89e72b7d5bb3de
Max MacNeill <d3n2c@unb.ca>
2024-08-11 20:59:55 -0300

    Added debug logging

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import discord
 import pymysql.cursors
 import settings
+import logging
 
 # sample code
+
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -21,4 +24,4 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('world!')
 
-client.run(settings.BOTTOKEN)
+client.run(settings.BOTTOKEN, log_handler=handler, log_level=logging.DEBUG)
